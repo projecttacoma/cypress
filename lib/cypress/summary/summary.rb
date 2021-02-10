48d5c3eb0ec0fdb5efce35cd65851047832a9e6f
David S Czulada <dczulada@mitre.org>
2021-02-10 10:27:54 -0500

    use helper function to determine what extension is being rendered

--- lib/cypress/summary/summary.rb
+++ lib/cypress/summary/summary.rb
@@ -14,11 +14,11 @@ class Summary < Mustache
     @relevant_entries.select { |pe| !['Patient', 'MeasureReport'].include? pe.resource.resourceType }.map(&:resource)
   end
 
-  def check_race(url)
-    render(url) == "http://hl7.org/fhir/us/core/StructureDefinition/us-core-race"
+  def race_extension?
+    self[:url] == "http://hl7.org/fhir/us/core/StructureDefinition/us-core-race"
   end
 
-  def check_ethnicity(url)
-    render(url) == "http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity"
+  def ethnicity_extension?
+    self[:url] == "http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity"
   end
 end
