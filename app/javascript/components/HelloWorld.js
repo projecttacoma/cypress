323ec1066ef7acd140517a3ab345941ecdb18d67
David S Czulada <dczulada@mitre.org>
2021-02-12 14:04:49 -0500

    Bring in fhir-viz

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,10 +1,28 @@
 import React from "react"
 import PropTypes from "prop-types"
+import ObjectInspector from "react-object-inspector"
+import { 
+  PatientVisualizer,
+  ConditionsVisualizer,
+  ObservationsVisualizer,
+  ReportsVisualizer,
+  MedicationsVisualizer,
+  AllergiesVisualizer,
+  CarePlansVisualizer,
+  ProceduresVisualizer,
+  EncountersVisualizer,
+  ImmunizationsVisualizer,
+  DocumentReferencesVisualizer
+} from 'fhir-visualizers';
+
 class HelloWorld extends React.Component {
   render () {
+  	let data = { /* ... */ };
     return (
       <React.Fragment>
-        Greeting: {this.props.greeting}
+        <div><PatientVisualizer patient={this.props.patient} /></div>
+        <div>FHIR Resource: <ObjectInspector data={ this.props.data } /></div>
+        <div>Errors: <ObjectInspector data={ this.props.errors } /></div>
       </React.Fragment>
     );
   }
